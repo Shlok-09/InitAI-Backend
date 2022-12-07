9f0cb8e506f1817060f560533a12a9d060bd9572
Sairaaj Surve <sairaaj26@gmail.com>
2022-12-07 20:27:32 +0530

    add multer and 404 route

--- services/ExpressApp.js
+++ services/ExpressApp.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const cors = require('cors')
-const { BlogRouter, UserRouter } = require('../routes')
+const multer = require('multer')
+const path = require('path')
+
+const { BlogRouter, UserRouter} = require('../routes')
+
+/******************* multer setup ********************** */
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './images')
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
 
 module.exports.App = async (app) => {
     app.use(express.json());
@@ -9,6 +28,13 @@ module.exports.App = async (app) => {
     
     app.use('/api/v1/blog', BlogRouter)
     app.use('/api/v1/user', UserRouter)
+    app.use('/api/v1/image', upload.single('image'), async (req, res, next) => {
+        return res.status(200).json({message: "Image successfully uploaded"})
+    })
+
+    app.use('*', async (req, res, next) => {
+        return res.status(404).send('<h1>404 Not Found</h1>')
+    })
 
     return app;
 }
