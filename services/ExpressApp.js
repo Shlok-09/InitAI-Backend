e06ef942cfb0d1ad7f6ad2a8edba2ee80625ff51
Shlok-09 <100953552+Shlok-09@users.noreply.github.com>
2022-12-28 13:54:53 +0530

    Update ExpressApp.js

--- services/ExpressApp.js
+++ services/ExpressApp.js
@@ -2,18 +2,19 @@ const express = require('express')
 const cors = require('cors')
 
 const { BlogRouter, UserRouter, ImageRouter, ProjectRouter, FileRouter, LectureRouter } = require('../routes')
+url = 'https://smoggy-worm-windbreaker.cyclic.app'
 
 module.exports.App = async (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cors())
 
-    app.use('/api/v1/blog', BlogRouter)
-    app.use('/api/v1/user', UserRouter)
-    app.use('/api/v1/image', ImageRouter)
-    app.use('/api/v1/project', ProjectRouter)
-    app.use('/api/v1/file', FileRouter)
-    app.use('/api/v1/lecture', LectureRouter)
+    app.use(url + '/api/v1/blog', BlogRouter)
+    app.use(url + '/api/v1/user', UserRouter)
+    app.use(url + '/api/v1/image', ImageRouter)
+    app.use(url + '/api/v1/project', ProjectRouter)
+    app.use(url + '/api/v1/file', FileRouter)
+    app.use(url + '/api/v1/lecture', LectureRouter)
 
     app.use('*', async (req, res, next) => {
         return res.status(404).send('<h1>404 Not Found</h1>')
