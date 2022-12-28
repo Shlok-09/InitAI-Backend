ea6055c08d08d523b4d40b0ac47c34e507e00003
Shlok-09 <100953552+Shlok-09@users.noreply.github.com>
2022-12-28 17:15:41 +0530

    Update ExpressApp.js

--- services/ExpressApp.js
+++ services/ExpressApp.js
@@ -9,12 +9,12 @@ module.exports.App = async (app) => {
     app.use(express.urlencoded({ extended: true }));
     app.use(cors())
 
-    app.use(url + '/api/v1/blog', BlogRouter)
-    app.use(url + '/api/v1/user', UserRouter)
-    app.use(url + '/api/v1/image', ImageRouter)
-    app.use(url + '/api/v1/project', ProjectRouter)
-    app.use(url + '/api/v1/file', FileRouter)
-    app.use(url + '/api/v1/lecture', LectureRouter)
+    app.use(url + 'api/v1/blog', BlogRouter)
+    app.use(url + 'api/v1/user', UserRouter)
+    app.use(url + 'api/v1/image', ImageRouter)
+    app.use(url + 'api/v1/project', ProjectRouter)
+    app.use(url + 'api/v1/file', FileRouter)
+    app.use(url + 'api/v1/lecture', LectureRouter)
 
     app.use('*', async (req, res, next) => {
         return res.status(404).send('<h1>404 Not Found</h1>')
