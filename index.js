29249dd568f2f647dcc206c4c0231a7d1f993b70
Shlok-09 <100953552+Shlok-09@users.noreply.github.com>
2022-12-29 19:37:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ const StartServer = async () => {
 
     await App(app)
 
-    app.get('/', async (req, res, next) => {
-        return res.status(200).send("<h1>Init Backend Index</h1>")
-    })
+    
 
     app.listen(PORT, () => {
         console.log(`Listening at port ${PORT}`)
