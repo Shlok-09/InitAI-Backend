6c68db4fef88d661877e136c2acbfecefbb3f8a1
Shlok-09 <100953552+Shlok-09@users.noreply.github.com>
2023-02-13 13:35:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,7 @@ const PORT = process.env.PORT || 3000
 const MONGO_URI = process.env.MONGO_URI
 
 const StartServer = async () => {
-
-    if (!fs.existsSync('./images')) {
-        fs.mkdirSync('./images')
-    }
-
-    if (!fs.existsSync('./files')) {
-        fs.mkdirSync('./files')
-    }
-    
+  
     const app = express()
 
     await dbConnection(MONGO_URI)
